b430cea96bb8c3626c0b0061ff4cdbab35b6f6f9
Haley <haley2153@hotmail.com>
2023-06-12 16:32:04 -0700

    Add caching workload

---
+++ make_workloads.sh
@@ -0,0 +1,3 @@
+python3.9 elastic/logs/custom_workflows.py --multiplier 30 --outfolder custom/30
+python3.9 elastic/logs/custom_workflows.py --multiplier 90 --outfolder custom/90
+python3.9 elastic/logs/custom_workflows.py --fixed_duration 90 --size_min 500 --outfolder custom/90/norequestcache
