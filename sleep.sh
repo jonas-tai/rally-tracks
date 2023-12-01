ffadd483def6d916ffbabac6632bf8656fb00419
Haley <haley2153@hotmail.com>
2023-11-30 20:52:18 -0800

    Add sleep.sh function

---
+++ sleep.sh
@@ -0,0 +1,16 @@
+while getopts c:m:t:s:l: flag; do
+    case "${flag}" in
+    c) CPU=${OPTARG} ;;
+    m) MEMORY=${OPTARG} ;;
+    t) TIME=${OPTARG} ;;
+    s) SLEEP=${OPTARG} ;;
+    l) LIMIT=${OPTARG} ;;
+    esac
+done
+
+STEP_SIZE=$((TIME + SLEEP))
+
+for ((i = 0; i < ${LIMIT}; i += STEP_SIZE)); do
+    stress -c ${CPU} -t ${TIME} -m ${MEMORY} -i ${MEMORY} -d ${MEMORY}
+    sleep ${SLEEP}s
+done
