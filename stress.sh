25ea8ec57956f7c746ddfbef7f2bd1ce7ca97c0a
Haley <haley2153@hotmail.com>
2023-12-02 11:36:43 -0800

    Update stress.sh to include disk ops and sleep

--- stress.sh
+++ stress.sh
@@ -1,21 +1,19 @@
-while getopts c:m:t:s:l: flag; do
+while getopts c:m:t:s:l:o: flag; do
     case "${flag}" in
     c) CPU=${OPTARG} ;;
     m) MEMORY=${OPTARG} ;;
     t) TIME=${OPTARG} ;;
     s) SLEEP=${OPTARG} ;;
     l) LIMIT=${OPTARG} ;;
+    o) OFFSET=${OPTARG} ;;
     esac
 done
 
 STEP_SIZE=$((TIME + SLEEP))
 
-# for ((i = 0; i < ${LIMIT}; i += STEP_SIZE)); do
-#     stress -c ${CPU} -t ${TIME} -m ${MEMORY} -i ${MEMORY} -d ${MEMORY}
-#     sleep ${SLEEP}s
-# done
+sleep ${OFFSET}s
 
 for ((i = 0; i < ${LIMIT}; i += STEP_SIZE)); do
-    stress-ng --matrix ${CPU} -t ${TIME}
+    stress-ng --matrix ${CPU} -t ${TIME} --copy-file ${CPU}
     sleep ${SLEEP}s
 done
