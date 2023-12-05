de3f2dc02762018c60689688aed2b1d7b84cd6a9
Haley (Compute Canada Node) <haleyli@cs.ubc.ca>
2023-12-05 01:21:06 +0000

    Commit December 4 experiments of RL with uniform caching and stressed nodes

--- motivation_early.sh
+++ motivation_early.sh
@@ -7,14 +7,15 @@ OUT_FOLDER="elastic/logs/workflows/custom/microbenchmarks_early"
 
 MICROBENCHMARK_TIME=900
 
-HIGH_LOAD="--draw_size True --zipf 1 --clients ${TOTAL_CLIENTS_HIGH_LOAD} --target_clients ${TOTAL_CLIENTS_HIGH_LOAD} --pareto 0.63 --size_max 5 --request_range 20 --load_period 10 --min_load 1 --size_multiplier 1 --draw_size_zero"
-HIGH_CACHE_SKEWED="--draw_size True --zipf 2 --clients ${TOTAL_CLIENTS_MED_LOAD} --target_clients ${TOTAL_CLIENTS_HIGH_LOAD} --pareto 0.6 --size_max 1 --request_range 30  --load_period 10 --min_load 1 --size_multiplier 1 --draw_size_zero --num_workflow_types 2"
-HIGH_CACHE_UNIFORM="--draw_size True --zipf 0 --clients ${TOTAL_CLIENTS_MED_LOAD} --target_clients ${TOTAL_CLIENTS_HIGH_LOAD} --pareto 0.6 --size_max 1 --request_range 30  --load_period 10 --min_load 1 --size_multiplier 1 --draw_size_zero --num_workflow_types 2"
-STRESSED_NODES="--draw_size True --zipf 1 --clients ${TOTAL_CLIENTS_MED_LOAD} --target_clients ${TOTAL_CLIENTS_HIGH_LOAD} --pareto 0.63 --size_max 5 --request_range 20 --load_period 10 --min_load 1 --size_multiplier 1 --draw_size_zero"
+HIGH_LOAD="--draw_size True --zipf 1 --clients ${TOTAL_CLIENTS_HIGH_LOAD} --target_clients ${TOTAL_CLIENTS_HIGH_LOAD} --pareto 0.63 --size_max 5 --request_range 60 --load_period 10 --min_load 1 --size_multiplier 1 --draw_size_zero"
+HIGH_CACHE_SKEWED="--draw_size True --zipf 2 --clients ${TOTAL_CLIENTS_MED_LOAD} --target_clients ${TOTAL_CLIENTS_HIGH_LOAD} --pareto 0.6 --size_max 1 --request_range 60  --load_period 10 --min_load 1 --size_multiplier 1 --draw_size_zero --num_workflow_types 2"
+HIGH_CACHE_UNIFORM="--draw_size True --zipf 0 --clients ${TOTAL_CLIENTS_MED_LOAD} --target_clients ${TOTAL_CLIENTS_HIGH_LOAD} --pareto 0.6 --size_max 1 --request_range 60  --load_period 10 --min_load 1 --size_multiplier 1 --draw_size_zero --num_workflow_types 2"
+STRESSED_NODES="--draw_size True --zipf 1 --clients ${TOTAL_CLIENTS_MED_LOAD} --target_clients ${TOTAL_CLIENTS_HIGH_LOAD} --pareto 0.63 --size_max 2 --request_range 50 --load_period 10 --min_load 1 --size_multiplier 1"
 
 SEED="1"
 
-python make_workload.py ${HIGH_LOAD} --out_folder ${OUT_PATH}/high_load --mode 'n' --max_workload_time ${MICROBENCHMARK_TIME} --seed ${SEED}
+OUT_PATH="elastic/logs/workflows/custom/microbenchmarks_early"
+
 python make_workload.py ${HIGH_CACHE_SKEWED} --out_folder ${OUT_PATH}/high_cache_skewed --mode 'n' --max_workload_time ${MICROBENCHMARK_TIME} --seed ${SEED}
-python make_workload.py ${HIGH_CACHE_UNIFORM} --out_folder ${OUT_PATH}/high_evict_uniform --mode 'n' --max_workload_time ${MICROBENCHMARK_TIME} --seed ${SEED}
+python make_workload.py ${HIGH_CACHE_UNIFORM} --out_folder ${OUT_PATH}/high_cache_uniform --mode 'n' --max_workload_time ${MICROBENCHMARK_TIME} --seed ${SEED}
 python make_workload.py ${STRESSED_NODES} --out_folder ${OUT_PATH}/stressed_nodes --mode 'n' --max_workload_time ${MICROBENCHMARK_TIME} --seed ${SEED}
